72b817da90d9fd9d8bd915aedd4e698662839b54
mananwason <manan13056@iiitd.ac.in>
2015-08-28 15:49:58 +0530

    root and ip states retrieved

--- app/src/main/java/com/adbwireless/mananwason/adbwirelessnoroot/MainActivity.java
+++ app/src/main/java/com/adbwireless/mananwason/adbwirelessnoroot/MainActivity.java
@@ -1,16 +1,92 @@
 package com.adbwireless.mananwason.adbwirelessnoroot;
 
 import android.app.Activity;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "ROOT";
+
+    TextView ip_address;
+
+    /**
+     * Checks if the device is rooted.
+     * @return <code>true</code> if the device is rooted, <code>false</code> otherwise.
+     */
+    public static boolean isRooted() {
+
+        boolean fileExists = false;
+        // get from build info
+        String buildTags = android.os.Build.TAGS;
+        if (buildTags != null && buildTags.contains("test-keys")) {
+            return true;
+        }
+
+        // check if /system/app/Superuser.apk is present
+        try {
+            File file = new File("/system/app/Superuser.apk");
+            if (file.exists()) {
+                Log.d(TAG, "file exists");
+                // try executing commands
+                fileExists = true;
+            } else {
+                Log.d(TAG, "file doesn't exist");
+                fileExists = false;
+            }
+        } catch (Exception e1) {
+            // ignore
+        }
+
+        return fileExists && (canExecuteCommand("/system/xbin/which su")
+                || canExecuteCommand("/system/bin/which su") || canExecuteCommand("which su"));
+    }
+
+    // executes a command on the system
+    private static boolean canExecuteCommand(String command) {
+        boolean executedSuccesfully;
+        try {
+            Runtime.getRuntime().exec(command);
+            executedSuccesfully = true;
+        } catch (Exception e) {
+            executedSuccesfully = false;
+        }
+
+        return executedSuccesfully;
+    }
+
+    public String getIpAddress() {
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
+        return String.format("%d.%d.%d.%d", (ipAddress & 0xff), (ipAddress >> 8 & 0xff),
+                (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ip_address = (TextView) findViewById(R.id.ip_address);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isRooted()) {
+                    ip_address.setText(getIpAddress() + " rooted");
+                } else {
+                    ip_address.setText("The device is not rooted");
+                    Log.d("ROOT", "not rooted");
+                }
+            }
+        });
     }
 
     @Override
@@ -26,7 +102,6 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
